cf18c16c31b255e889a43afbf2b0246e452486b5
lenapy <rashchyk1351@gmail.com>
2017-11-20 18:37:34 +0200

    second task is done

---
+++ tasks/draw_ladder.py
@@ -0,0 +1,17 @@
+"""
+At the entrance the program will receive the number of stairs.
+The goal is to print the ladder using the space characters " " and the "#" grids.
+"""
+
+import sys
+stairs = sys.argv[1]
+stairs_digit = int(stairs)
+whitespace = ' '
+grid = '#'
+number_of_grid = 1
+for stair in range(stairs_digit):
+    line = whitespace*(stairs_digit-1) + grid * number_of_grid
+    print(''.join(line))
+    stairs_digit -= 1
+    number_of_grid += 1
+
