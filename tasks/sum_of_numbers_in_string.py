9a18cd43fe64cf7c2329d16c530d244766299a96
lenapy <rashchyk1351@gmail.com>
2017-11-20 17:34:12 +0200

    removed the extra code

--- tasks/sum_of_numbers_in_string.py
+++ tasks/sum_of_numbers_in_string.py
@@ -1,23 +1,18 @@
 """
 найти сумму цифр, из которых состоит строка.
 """
-import sys
 
-
-digit_str = sys.argv[0]
-print(digit_str)
-
-
-while True:
-    input_str = input()
-    if not input_str.isdigit():
-        print('В строке должны быть только числа')
-    else:
-        sum_num = 0
-        for num in input_str:
-            sum_num += int(num)
-        print(sum_num)
-        break
+if __name__ == '__main__':
+    while True:
+        input_str = input()
+        if not input_str.isdigit():
+            print('В строке должны быть только числа')
+        else:
+            sum_num = 0
+            for num in input_str:
+                sum_num += int(num)
+            print(sum_num)
+            break
 
 
 
